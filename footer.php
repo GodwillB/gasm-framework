f36a0024f0cfcfbca1cc17cf5b5171b9e8cf0ed5
GodwillB <iamgodwillb@gmail.com>
2020-08-25 03:09:59 +0300

    5

---
+++ footer.php
@@ -0,0 +1,15 @@
+<!-- Footer content start  -->
+<!-- Footer content end -->
+
+<!-- JS, Popper.js, and jQuery -->
+
+	<script src="assets/js/three.min.js"></script>
+	<script src="assets/js/TweenMax.min.js"></script>
+	<script src="dist/hover.umd.js"></script>
+	<script type="text/javascript" src="assets/js/jquery-3.3.1.min.js"></script>
+	<script type="text/javascript" src="vendor/bootstrap/js/bootstrap.min.js"></script>
+	<script type="text/javascript" src="vendor/popper/popper.min.js"></script>
+	<script type="text/javascript" src="vendor/owl/js/owl.carousel.min.js"></script>
+	<script type="text/javascript" src="assets/js/main.js"></script>
+</body>
+</html>
--- index.php
+++ index.php
@@ -1,26 +1,7 @@
-<!DOCTYPE html>
-<html>
-<head>
-	<title></title>
-	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
-	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
+<!-- Header + Navbar -->
+<?php require_once "nav.php"; ?>
 
-</head>
-<body>
+<!-- Body content  -->
 
-
-
-
-<!-- JS, Popper.js, and jQuery -->
-<<<<<<< HEAD
-<script src="assets/js/three.min.js"></script>
-<script src="assets/js/TweenMax.min.js"></script>
-<script src="dist/hover.umd.js"></script>
-=======
-<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
-<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
-<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js" integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV" crossorigin="anonymous"></script>
->>>>>>> 5418d8974047c21c2caa1afa4ff254b86a10f7a1
-
-</body>
-</html>
+<!-- Add footer -->
+<?php require_once "footer.php"; ?>
